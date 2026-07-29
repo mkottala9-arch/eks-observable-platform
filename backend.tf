1f889af5c903fdee8745dfdad6209acbfbb5466e
Mohan Krishna Kottala <mkottala9@gmail.com>
2026-07-29 07:11:11 +0000

    point backend at new account bucket, switch region to ap-south-2

--- backend.tf
+++ backend.tf
@@ -5,9 +5,9 @@ terraform {
   # use_lockfile -> native S3 locking (TF 1.10+), no DynamoDB table needed.
   # Note: backend block can't use variables, values must be hardcoded here.
   backend "s3" {
-    bucket       = "eks-observable-platform-tfstate-krishna767371"
+    bucket       = "eks-observable-platform-tfstate-krishna756808"
     key          = "eks-observable-platform/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "ap-south-2"
     encrypt      = true
     use_lockfile = true
   }
